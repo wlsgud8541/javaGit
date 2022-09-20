965caa05c3da371b15a533a269416e169725710e
15B <15B@DESKTOP-PLDUHJG>
2022-09-20 18:55:15 +0900

    과제 추가

---
+++ jstudy/src/oday01/Ex12.java
@@ -0,0 +1,32 @@
+package oday01;
+
+public class Ex12 {
+
+/*
+
+1. 
+
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+
+
+2. 
+
+1 2 3 4 5
+6 7 8 9 10
+11 12 13 14 15
+16 17 18 19 20
+21 22 23 24 25
+
+
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
