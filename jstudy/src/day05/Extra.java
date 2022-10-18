584e45dc647336284eeb639854842d1a606efd35
전진형 <@DESKTOP-NDL73D7>
2022-10-18 18:18:50 +0900

    commit

---
+++ jstudy/src/day05/Extra.java
@@ -0,0 +1,32 @@
+package day05;
+
+/*
+ * 랜덤하게 0 ~ 100 사이의 정수를 발생시켜서
+ * 그 정수를 점수로 갖는 학생의 학점을 출력해주는 프로그램을 장석하세요.
+ * 학점은	
+ * 
+ * 		  F < 60
+ * 	60 <= D < 70
+ *  70 <= C < 80
+ *  80 <= B < 90
+ *  90 <= A <= 100
+ *  
+ *  으로 처리하는 것으로 하고
+ *  3항 조건 연산자로 처리하세요.
+ *  
+ * */
+public class Extra {
+	public static void main(String[] args) {
+		
+		int num = (int)(Math.random()*101);
+		
+		String result = (num < 60) ? "F" : (
+							(num >= 60 && num < 70) ? "D" : (
+									(num >= 70 && num < 80) ? "C" : (
+											(num >= 80 && num < 90) ? "B" :
+												(num >= 90 && num <= 100) ? "A" : "입력 값을 벗어 났습니다."))); 
+		
+		System.out.println("획득한 점수 :"+num+" / 학점 :"+result);
+		
+	}
+}
