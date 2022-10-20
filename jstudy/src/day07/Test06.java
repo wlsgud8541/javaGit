2b041af50cf9f526245ab84098c56196e7344ae2
15B <15B@DESKTOP-PLDUHJG>
2022-10-20 18:06:30 +0900

    commit

---
+++ jstudy/src/day07/Test06.java
@@ -0,0 +1,15 @@
+package day07;
+
+import java.util.Arrays;
+
+public class Test06 {
+	public static void main(String[] args) {
+		Object[] obj = new Object[3];
+		
+		obj[0] = "jennie";
+		obj[1] = 27;
+		obj[2] = 'F';
+		
+		System.out.println(Arrays.toString(obj));
+	}
+}
